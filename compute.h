a014d826ee6b43949770b06074a903351e623d24
dr_squanch <79220528+caadams4@users.noreply.github.com>
2023-05-12 15:42:12 -0400

    Update compute.h

--- compute.h
+++ compute.h
@@ -1,1 +1,6 @@
 void compute();
+__global__ void accelEffectsInitialize(vector3 *values, vector3 **accels);
+
+__global__ void computePairwiseAccels(double d_mass,vector3* accels_sum,vector3 **accels,vector3 *d_hPos);
+
+__global__ void sumAccels(vector3 *d_hPos, vector3 *d_hVel);
